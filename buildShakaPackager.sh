5089e3299f2cd6ac4829398a67fe6d1a48cedc6b
Caitlin O'Callaghan <cocallaghan@fb.com>
2021-08-03 22:39:23 -0700

    Update Shaka Packager installation script

--- buildShakaPackager.sh
+++ buildShakaPackager.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # This script will remove and reinstall Shaka Packager
+# The version of Shaka Packager being installed is compatible with x86_64 and aarch64
 
-# Shaka Packager with LL-DASH and ARM support
+# Shaka Packager with LL-DASH
 export SHAKA_PACKAGER_SOURCE="CaitlinOCallaghan"
-export SHAKA_PACKAGER_COMMIT="a03b52894bccc149718a37a0a53d5b6e142cebc0"
-export GYP_DEFINES="clang=0 use_allocator=none"
+export SHAKA_PACKAGER_COMMIT="45e92b4b9fdfdcb6842f891886d09f52ecb2ef73"
 
 export PATH="$PATH:$PWD/depot_tools"
 rm -rf shaka-packager
@@ -13,7 +13,7 @@ mkdir shaka-packager
 cd shaka-packager
 # grab the fork that contains ULL work
 gclient config "https://github.com/$SHAKA_PACKAGER_SOURCE/shaka-packager.git" --name=src --unmanaged
-# checkout the LL-DASH and ARM support branch
+# checkout the LL-DASH support branch
 # NOTE: use "gclient sync -r {commit_hash}" to checkout a specific commit or branch
 # otherwise, use "gclient sync" to checkout the main branch
 gclient sync -r ${SHAKA_PACKAGER_COMMIT}
