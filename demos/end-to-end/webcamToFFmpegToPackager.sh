51726fbe0031b069162df283be44d864c86b744a
Caitlin O'Callaghan <cocallaghan@fb.com>
2021-08-08 21:34:03 -0700

    Correct segment numbering

--- demos/end-to-end/webcamToFFmpegToPackager.sh
+++ demos/end-to-end/webcamToFFmpegToPackager.sh
@@ -70,7 +70,7 @@ packager \
    --v=0 \
    --io_block_size 65536 \
    --nogenerate_sidx_in_media_segments \
-   in=pipe1,stream=video,init_segment='http://'${IP}':'${PORT}'/'${SERVER_UPLOAD_DIR}'/'${OUTPUT_DIR}'/'${OUTPUT_SEG_NAME}'_init.m4s',segment_template='http://'${IP}':'${PORT}'/'${SERVER_UPLOAD_DIR}'/'${OUTPUT_DIR}'/'${OUTPUT_SEG_NAME}'_$Number$.m4s' \
+   in=pipe1,stream=video,init_segment='http://'${IP}':'${PORT}'/'${SERVER_UPLOAD_DIR}'/'${OUTPUT_DIR}'/'${OUTPUT_SEG_NAME}'_init.m4s',segment_template='http://'${IP}':'${PORT}'/'${SERVER_UPLOAD_DIR}'/'${OUTPUT_DIR}'/'${OUTPUT_SEG_NAME}'_$Number%05d$.m4s' \
    --segment_duration 5 \
    --is_low_latency_dash=true \
    --utc_timings "urn:mpeg:dash:utc:http-xsdate:2014"="https://time.akamai.com/?iso" \
