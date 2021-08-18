8a727bf828aa8ddfc66e01301d4c592677fa943e
Caitlin O'Callaghan <cocallaghan@fb.com>
2021-08-17 22:54:25 -0700

    Use chunked transfer encoding for AWS proxy

--- demos/server/launchAwsProxy.sh
+++ demos/server/launchAwsProxy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-PORT=8080
-AWS_REGION='us-west-2'
-BUCKET_NAME='shaka_ull'
-CLOUD_STORAGE='mediastore' # Please select 'mediastore' or 's3'
+export PORT=8080
+export AWS_REGION='us-west-2'
+export BUCKET_NAME='shaka_ull'
+export CLOUD_STORAGE='mediastore' # Please select 'mediastore' or 's3'
 
-LAUNCH_SERVER_DIR=$PWD
-SHIM_DIR='../../s3-upload-proxy'
+export LAUNCH_SERVER_DIR=$PWD
+export SHIM_DIR='../../s3-upload-proxy'
 
 echo "Launching ${CLOUD_STORAGE} proxy for '${BUCKET_NAME}' on port ${PORT}"
 
-export UPLOAD_DRIVER=${CLOUD_STORAGE} BUCKET_NAME=${BUCKET_NAME} REGION_NAME=${AWS_REGION} HTTP_PORT=${PORT}
+export UPLOAD_DRIVER=${CLOUD_STORAGE} BUCKET_NAME=${BUCKET_NAME} AWS_REGION=${AWS_REGION} HTTP_PORT=${PORT} MEDIASTORE_CHUNKED_TRANSFER=true 
 cd ${SHIM_DIR}
 go build -o s3-upload-proxy
 cd ${LAUNCH_SERVER_DIR}
